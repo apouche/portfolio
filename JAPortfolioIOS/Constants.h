419010ac67606a7e58d4eaec4655dfdbb074bd76
apouche <johan.attali@gmail.com>
2013-04-21 17:20:29 +0200

    adding file Constants.h with view frame macros

---
+++ JAPortfolioIOS/Constants.h
@@ -0,0 +1,14 @@
+//
+//  JAAppDelegate.h
+//  JAPortfolioIOS
+//
+//  Created by Johan Attali on 4/18/13.
+//  Copyright (c) 2013 Johan Attali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define JAViewX(v) (v).frame.origin.x
+#define JAViewY(v) (v).frame.origin.y
+#define JAViewW(v) (v).frame.size.width
+#define JAViewH(v) (v).frame.size.height
