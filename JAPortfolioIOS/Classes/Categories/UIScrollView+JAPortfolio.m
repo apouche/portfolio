228fc3cc7873d000dd4d7c6c9afda1e10e899fa1
apouche <johan.attali@gmail.com>
2013-04-23 21:58:32 +0200

    adding uiscrollview category

---
+++ JAPortfolioIOS/Classes/Categories/UIScrollView+JAPortfolio.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+JAPortfolio.m
+//  JAPortfolioIOS
+//
+//  Created by Johan Attali on 4/23/13.
+//  Copyright (c) 2013 Johan Attali. All rights reserved.
+//
+
+#import "UIScrollView+JAPortfolio.h"
+
+@implementation UIScrollView (JAPortfolio)
+
+@end
