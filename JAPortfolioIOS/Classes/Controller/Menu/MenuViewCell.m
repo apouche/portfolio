ddfdb587a995c22cb1b5e7be7a589b52c19c3a79
apouche <johan.attali@gmail.com>
2013-04-27 21:38:21 +0200

    adding slight sperator views

--- JAPortfolioIOS/Classes/Controller/Menu/MenuViewCell.m
+++ JAPortfolioIOS/Classes/Controller/Menu/MenuViewCell.m
@@ -20,14 +20,27 @@
         self.backgroundView.backgroundColor = kMenuViewCellBGColor;
         self.contentView.backgroundColor	= kMenuViewCellBGColor;
 		
+		UIView* vTop	= [[UIView alloc] initWithFrame:CGRectMake(0, 0, JAViewW(self), 1)];
+		UIView* vBottom = [[UIView alloc] initWithFrame:CGRectMake(0, JAViewH(self)-1, JAViewW(self), 1)];
+
+		vTop.backgroundColor	= JARGB(50,49,50);
+		vBottom.backgroundColor = JARGB(27,27,27);
+
 		_titleLabel = [[UILabel alloc] initWithFrame:CGRectOffset(self.bounds, 30, 0)];
 		_titleLabel.font			= [UIFont fontWithName:@"Verdana" size:15];
 		_titleLabel.backgroundColor = [UIColor clearColor];
 		_titleLabel.textColor		= JARGB(224,224,224);
 		
+		[self addSubview:vTop];
+		[self addSubview:vBottom];
+
+		[self addSubview:_titleLabel];
+
 		[self addSubview:_titleLabel];
 		
 		[_titleLabel release];
+		[vTop release];
+		[vBottom release];
     }
     return self;
 }
